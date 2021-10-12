4a46ee9ad7005aa53d7cce248d29a7216ac082ec
NicholasDay1992 <71455485+NicholasDay1992@users.noreply.github.com>
2021-10-12 15:10:19 +0100

    update module

---
+++ app211-enrollments/Module.java
@@ -0,0 +1,36 @@
+
+/**
+ * Write a description of class Module here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Module
+{
+    // instance variables - replace the example below with your own
+    private String code;
+    
+    private String title;
+    
+    private int credit;
+
+    /**
+     * Constructor for objects of class Module
+     */
+    public Module(String code)
+    {
+        // initialise instance variables
+        this.code = code;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void sampleMethod()
+    {
+        // put your code here
+    }
+}
